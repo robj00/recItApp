52f370ba9c5e286efb8454e17c3ba818834e03e1
Rob Palmer <rjpalmer15@gmail.com>
2017-06-14 14:21:32 -0400

    changed to relative protocols

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function queryAPITasty (searchText, searchType, callback) {
 		limit: '20',
 		k: '271516-Recommen-DHVL11EC'
 	};
-	$.getJSON('http://www.tastekid.com/ask/ws', params, callback);
+	$.getJSON('//www.tastekid.com/ask/ws', params, callback);
 
 }
 
